dcef607a5276c5dcb7f097efbcc8d98f566ff8c7
Aravind Kumar <ara3194@gmail.com>
2025-01-08 21:38:44 +1100

    Working Navbar

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container } from 'react-bootstrap';
 import { AuthProvider } from "./contexts/authContexts";
+import Header from "./pages/Header"; // Import Header
 import LoginSignup from "./pages/LoginSignup";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginSignup />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </Router>
+      <BrowserRouter>
+        {/* Header always visible */}
+        <div>
+          <Header />
+          <Container>
+            <Routes>
+              <Route path="/" element={<LoginSignup />} />
+              <Route path="/home" element={<Home />} />
+          {/* Add other routes as needed */}
+            </Routes>
+          </Container>
+        </div>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
---
+++ dashboard-app/src/pages/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+
+const Header = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/";
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        {/* Company Name */}
+        <Navbar.Brand as={Link} to="/">
+          Advanced Chain Technologies
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {isLoginPage ? (
+              // No additional links on the login page
+              <></>
+            ) : (
+              // Show navigation links after login
+              <>
+                <Nav.Link as={Link} to="/home">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/machines">
+                  Machines
+                </Nav.Link>
+                <Nav.Link as={Link} to="/production">
+                  Production
+                </Nav.Link>
+                <Nav.Link as={Link} to="/clients">
+                  Clients
+                </Nav.Link>
+                <Nav.Link as={Link} to="/log">
+                  Log
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
