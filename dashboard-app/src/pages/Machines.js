8d5f2952c21aaf5c00dc0f0ef0c8fca61b38061d
Aravind Kumar <ara3194@gmail.com>
2025-01-13 20:50:07 +1100

    Minor gauge changes-2

--- dashboard-app/src/pages/Machines.js
+++ dashboard-app/src/pages/Machines.js
@@ -9,17 +9,17 @@ const Machines = ({ initialMachineData }) => {
 
   const getSubArcs = (totalCount) => [
     {
-      limit: totalCount * 0.2,
+      limit: totalCount * 0.25,
       color: "#EA4228",
       showTick: true,
     },
     {
-      limit: totalCount * 0.4,
+      limit: totalCount * 0.5,
       color: "#F58B19",
       showTick: true,
     },
     {
-      limit: totalCount * 0.6,
+      limit: totalCount * 0.75,
       color: "#F5CD19",
       showTick: true,
     },
@@ -68,7 +68,7 @@ const Machines = ({ initialMachineData }) => {
       <div style={{ display: "flex", justifyContent: "space-around", flexWrap: "wrap" }}>
         {Object.keys(machineData).map((machine, index) => (
           <div key={machine} style={{ textAlign: "center", margin: "20px" }}>
-            <h3>Machine {index + 1}</h3>
+            <h4>Machine {index + 1}</h4>
             <GaugeComponent
               value={machineData[machine].value}
               minValue={0}
@@ -76,6 +76,8 @@ const Machines = ({ initialMachineData }) => {
               arc={{
                 subArcs: getSubArcs(machineData[machine].totalCount),
               }}
+              // width={250} // Increased width
+              // height={250} // Increased height
               labels={{
                 valueLabel: {
                   format: (value) => `${value}/${machineData[machine].totalCount}`,
