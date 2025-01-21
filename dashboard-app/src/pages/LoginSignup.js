31aeab4478d8566abe0c642c44be344bbfb7608e
Aravind Kumar <ara3194@gmail.com>
2025-01-21 20:19:09 +1100

    Final changes v2 prod

--- dashboard-app/src/pages/LoginSignup.js
+++ dashboard-app/src/pages/LoginSignup.js
@@ -2,14 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, db } from "../firebase/firebase"; // Firebase configuration
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import {
-  collection,
-  doc,
-  setDoc,
-  getDocs,
-  query,
-  where,
-} from "firebase/firestore";
+import { collection, doc, setDoc, getDocs, query, where } from "firebase/firestore";
 import { Button, Form, Container } from "react-bootstrap";
 import "../styles/LoginSignup.css";
 
@@ -38,14 +31,14 @@ const LoginSignup = ({ setCurrentUser }) => {
 
         // Check if the password matches
         if (userData.password === password) {
-          // Navigate based on the user's role
+          // Set the current user
           const currentUserData = {
             email: userData.email,
             password: userData.password,
             role: userData.role,
           };
-          setCurrentUser(currentUserData); // Update the currentUser state
-          navigate("/home"); // Navigate to the common home screen
+          setCurrentUser(currentUserData);
+          navigate("/home"); // Navigate to the home screen
         } else {
           throw new Error("Incorrect password. Please try again.");
         }
@@ -59,27 +52,20 @@ const LoginSignup = ({ setCurrentUser }) => {
     }
   };
 
-  // Handle User Creation
   const handleCreateUser = async (e) => {
     e.preventDefault();
     setLoading(true);
     setError("");
 
     try {
-      // Firebase Authentication
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       const userId = userCredential.user.uid;
 
       // Save New User to Firestore with Default Role as "staff"
       const newUser = { email, password, role: "staff" };
-
       await setDoc(doc(usersCollection, userId), newUser);
       setCurrentUser(newUser);
-      navigate("/home"); // Redirect to home page after successful creation
+      navigate("/home");
     } catch (err) {
       setError(err.message);
     } finally {
@@ -90,10 +76,12 @@ const LoginSignup = ({ setCurrentUser }) => {
   return (
     <div className="login-page">
       <Container className="login-container">
-        <h1 className="text-center mb-4">Advanced Chain Technologies</h1>
+        <h2 className="text-center mb-4"><i className="fa fa-cogs"> Advanced Chain Technologies</i></h2>
         <Form onSubmit={isCreating ? handleCreateUser : handleLogin}>
           <Form.Group controlId="formBasicEmail" className="mb-3">
-            <Form.Label>Email</Form.Label>
+          <Form.Label className="form-label-custom">
+  <i className="fa fa-envelope"></i> Email Address
+</Form.Label>
             <Form.Control
               type="email"
               placeholder="name@example.com"
@@ -104,7 +92,7 @@ const LoginSignup = ({ setCurrentUser }) => {
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword" className="mb-3">
-            <Form.Label>Password</Form.Label>
+            <Form.Label><i className="fa fa-lock"></i> Password</Form.Label>
             <Form.Control
               type="password"
               placeholder="Password"
@@ -114,11 +102,11 @@ const LoginSignup = ({ setCurrentUser }) => {
             />
           </Form.Group>
 
-          {error && <p className="text-danger mt-3">{error}</p>}
+          {error && <p className="error-message">{error}</p>}
 
           <Button
             type="submit"
-            variant={isCreating ? "success" : "dark"}
+            variant={isCreating ? "success" : "info"}
             className="w-100 mt-4"
             disabled={loading}
           >
@@ -132,7 +120,7 @@ const LoginSignup = ({ setCurrentUser }) => {
           </Button>
 
           <Button
-            variant="link"
+            variant="primary"
             className="w-100 mt-3"
             onClick={() => setIsCreating((prev) => !prev)}
           >
